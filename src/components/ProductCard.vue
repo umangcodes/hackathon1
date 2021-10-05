2d6d0fe6d6ed21405f1ee58eec00924aade92ea2
Umang A <umanga.eng@gmail.com>
2021-10-05 19:17:30 -0400

    product card created and catalogue changed

--- src/components/ProductCard.vue
+++ src/components/ProductCard.vue
@@ -1,7 +1,17 @@
 <template>
-  <div>
-    <span>{{ data.img }}</span>
-    <span>{{ data.id }}</span>
+  <div :class="data.disable ? 'opacity-30' : ''">
+    <div>
+      <span class="text-xl font-bold">Serial #{{ data.id }}</span>
+    </div>
+    <div class="object-cover md:object-scale-down">
+      <img src="https://tailwindcss.com/img/placeholder-amber.svg" alt="NA" />
+    </div>
+    <div class="">
+      <span class="font-thin">Name </span>
+      <br />
+      <span class="font-thin">Power</span>
+    </div>
+    <div class="opacity-0 hover:opacity-100" v-if="data.disable">Locked</div>
   </div>
 </template>
 
--- src/views/Catalogue.vue
+++ src/views/Catalogue.vue
@@ -1,5 +1,22 @@
 <template>
-  <ProductCard v-for="product in products" :data="product" :key="product.id" />
+  <div class="m-5">
+    <div class="mb-16">
+      <span class="text-2xl font-light border-b">Our Range</span>
+    </div>
+    <div class="grid grid-cols-1 xl:grid-cols-8 lg:grid-cols-6 md:grid-cols-3">
+      <div v-for="product in products" :key="product.id">
+        <div>
+          <ProductCard :data="product" />
+        </div>
+      </div>
+    </div>
+    <button
+      class="border-2 rounded-full px-2 py-1 text-lg bg-gray-200 text-gray-700"
+      :onclick="buttonClick"
+    >
+      Load More
+    </button>
+  </div>
 </template>
 
 <script>
@@ -11,20 +28,59 @@ export default {
         {
           img: "abcd",
           id: 1,
+          disable: false,
         },
         {
           img: "xyz",
           id: 2,
+          disable: false,
         },
         {
           img: "asdf",
           id: 3,
+          disable: false,
+        },
+        {
+          img: "abcd",
+          id: 4,
+          disable: false,
+        },
+        {
+          img: "xyz",
+          id: 5,
+          disable: false,
+        },
+        {
+          img: "asdf",
+          id: 6,
+          disable: false,
+        },
+        {
+          img: "abcd",
+          id: 7,
+          disable: false,
+        },
+        {
+          img: "xyz",
+          id: 8,
+          disable: true,
+        },
+        {
+          img: "asdf",
+          id: 9,
+          disable: true,
         },
       ],
     };
   },
   components: {
     ProductCard,
+  },
+  methods: {
+    buttonClick() {
+      console.log("button pressed");
+      this.$router.push("/");
+    },
   },
 };
 </script>
