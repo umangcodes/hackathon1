527caa0aea33be708706ab7993f1e37e0e2e9302
Umang A <umanga.eng@gmail.com>
2021-10-11 23:08:42 -0400

    now home page has tech stack

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,22 @@
 <template>
-  <div class="home">
-    <h1>Home Page</h1>
+  <div class="h-screen relative border">
+    <div class="">
+      <h1>Home Page</h1>
+    </div>
+    <div class="">
+      <!-- relative bottom-0 -->
+      <Tech />
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-
+import Tech from "@/components/WelcomeComponents/Tech.vue";
 export default {
   name: "Home",
+  components: {
+    Tech,
+  },
 };
 </script>
