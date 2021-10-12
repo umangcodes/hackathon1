00e2691cac4ba541371571cdf3ff91e2e09c0b04
Umang A <umanga.eng@gmail.com>
2021-10-11 23:06:44 -0400

    tech component created for home page

---
+++ src/components/WelcomeComponents/Tech.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="mt-5 mb-5 p-2 absolute inset-x-0 bottom-0">
+    <span class="flex justify-center border-b">Our Tech Stack</span>
+    <div class="grid xl:grid-cols-12 sm:grid-cols-4 pt-5">
+      <div class="col-span-4 sm:col-span-0"></div>
+      <button>T1</button>
+      <button>T2</button>
+      <button>T3</button>
+      <button>T4</button>
+      <div class="col-span-4 sm:col-span-0"></div>
+    </div>
+  </div>
+</template>
+
+<script></script>
+
+<style></style>
