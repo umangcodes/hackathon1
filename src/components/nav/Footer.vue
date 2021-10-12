864e905cf67c50e2e968c173504f9f9f17a8f7a8
Umang A <umanga.eng@gmail.com>
2021-10-11 23:07:58 -0400

    util class chnaged from absolute to static bottom

--- src/components/nav/Footer.vue
+++ src/components/nav/Footer.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="absolute bottom-0 p-2 bg-gray-100 w-full flex flex-col">
+  <div class="static bottom-0 p-2 bg-gray-100 w-full flex flex-col">
     <div class="grid grid-cols-12 sm:grid-cols-8 border-b">
       <div class="col-span-4 sm:col-span-2"></div>
       <button>FB</button>
